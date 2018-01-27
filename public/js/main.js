fb559989c97a6446aff0d7724ac3432bab67ca8d
temusu <temusgaming@gmail.com>
2018-01-28 01:14:01 +0900

    first site deploy
    done all planned

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,43 @@
-const $bg = document.querySelectorAll('.bg')[0]
+// hover animation on links 
+document.querySelectorAll('.link').forEach(function(elem) {
+  console.dir(elem)
+  elem.addEventListener('mouseover', function(e) {
+    console.dir(e)
+    elem.parentNode.parentNode.classList.add('hover')
+  }, true)
+  elem.addEventListener('mouseleave', function() {
+    elem.parentNode.parentNode.classList.remove('hover')
+  }, true)
+})
 
-
-let pos = ( $bg.clientHeight - window.innerHeight )
-
-function onBgLoad() {
-  Velocity($bg,{
-    blur: [0, 4]
-    // scale: [1, 2]
-  },{
-    duration: pos*10,
-    queue: false
-  })
-  Velocity($bg,{
-    translateY: [-1*pos, pos/-2],
-    scale: [1, 1.1],
-    opacity: [1, 0]
-  },{
-    duration: pos*20,
-    easing: 'easeOutSine'
-  })
-}
-
-$bg.style.cssText = `transform: translatey(-${ (pos) }px)`
+document.addEventListener("DOMContentLoaded", function(){
+  setTimeout(function(){
+    document.querySelector('.bg.hide').classList.remove('hide')
+  }, 10)
+  setTimeout(function(){
+    document.querySelector('.site-title.hide').classList.remove('hide')
+  }, 100)
+  var siteNav = document.querySelectorAll('.site-nav .item')
+  var socialNav = document.querySelectorAll('.social-nav .item')
+  var siteNavCount = 0
+  var socialNavCount = 0
+  setTimeout(function() {
+    var siteNavInterval = setInterval(function(){
+      console.log(siteNavCount, siteNav.length)
+      siteNav[siteNavCount].classList.remove('hide')
+      siteNavCount++
+      if (siteNavCount === siteNav.length) clearTimeout(siteNavInterval)
+    }, 100)
+  }, 200)
+  setTimeout(function() {
+    var socialNavInterval = setInterval(function(){
+      socialNav[socialNavCount].classList.remove('hide')
+      socialNavCount++
+      if (socialNavCount === socialNav.length) clearTimeout(socialNavInterval)
+    }, 100)
+  }, 200)
+  
+  var inte = setInterval(function() {
+    document.querySelector('.logo-wrap').classList.toggle('hm')
+  }, 1000)
+});
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const app = express()
 // set post
 const port = process.env.PORT || 8080
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/public'))
 
 // routes
-app.get('', function(req, res) {
-  res.render('index')
-})
+// app.get('', function(req, res) {
+//   res.render('index')
+// })
 
 app.listen(port, function() {
   console.log('app running')
